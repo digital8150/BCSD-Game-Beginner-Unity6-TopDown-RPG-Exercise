983fe29a26a79800158468767a54aa892aa376a9
Digital <73296205+digital8150@users.noreply.github.com>
2025-04-23 21:53:51 +0900

    feature: Rpg Maker style player movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,6 +5,9 @@ public class Player : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     float h;
     float v;
+    bool isHorizontal;
+
+    public float Speed;
     Rigidbody2D rigid;
     void Start()
     {
@@ -21,11 +24,20 @@ public class Player : MonoBehaviour
     {
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
+
+        bool hDown = Input.GetButton("Horizontal");
+        bool vDown = Input.GetButton("Vertical");
+
+        if(hDown)
+            isHorizontal = true;
+        else if(vDown)
+            isHorizontal = false;
     }
 
     // FixedUpdate is called once per physics frame
     void FixedUpdate()
     {
-        rigid.linearVelocity = new Vector2(h, v);
+        Vector2 moveVect = isHorizontal ? new Vector2(h, 0) : new Vector2(0,v);
+        rigid.linearVelocity = moveVect * Speed;
     }
 }
